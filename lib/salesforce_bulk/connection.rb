95471d3af3dcd348a430749ccb8ed2cba84d5239
noornabee <noornabee@yahoo.com>
2014-11-04 12:26:26 +0400

    Update connection.rb

--- lib/salesforce_bulk/connection.rb
+++ lib/salesforce_bulk/connection.rb
@@ -13,7 +13,6 @@ module SalesforceBulk
       @session_id = nil
       @server_url = nil
       @instance = nil
-      puts "api_version #{api_version}"
       @@API_VERSION = api_version
       @@LOGIN_PATH = "/services/Soap/u/#{@@API_VERSION}"
       @@PATH_PREFIX = "/services/async/#{@@API_VERSION}/"
@@ -43,15 +42,9 @@ module SalesforceBulk
       response = post_xml(@@LOGIN_HOST, @@LOGIN_PATH, xml, headers)
       # response_parsed = XmlSimple.xml_in(response)
       response_parsed = parse_response response
-      
-      puts "response_parsed #{response_parsed}"
 
       @session_id = response_parsed['Body'][0]['loginResponse'][0]['result'][0]['sessionId'][0]
       @server_url = response_parsed['Body'][0]['loginResponse'][0]['result'][0]['serverUrl'][0]
-      
-      puts "session_id #{@session_id}"
-      
-      puts "server_url #{@server_url}"
       
       @instance = parse_instance()
 
@@ -67,11 +60,6 @@ module SalesforceBulk
         headers['X-SFDC-Session'] = @session_id;
         path = "#{@@PATH_PREFIX}#{path}"
       end
-      
-      puts "host #{host}"
-      puts "path #{path}"
-      puts "xml #{xml}"
-      puts "header #{headers}"
 
       https(host).post(path, xml, headers).body
     end
